5f72d8bde49f36dd36fdff811fe1fcf2ba659f36
Murilo Mothsin <murilo.mothsin@indiegogo.com>
2021-01-21 20:57:41 -0300

    Config mongo url

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 
+const mongo_url = process.env.MONGO_URL || 'mongodb://localhost/datamob-api';
+
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb://localhost/datamob-api')
+module.exports = mongoose.connect(mongo_url)
