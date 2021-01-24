1618d18a4ef669cc94383d17137314d8e8f5934a
Murilo Mothsin <murilo.mothsin@indiegogo.com>
2021-01-24 19:12:58 -0300

    Fix cors

--- src/config/server.js
+++ src/config/server.js
@@ -2,12 +2,14 @@ const port = process.env.PORT || 3003
 
 const bodyParser = require('body-parser')
 const express = require('express')
+var logger = require('morgan');
 const server = express()
-const allowCors = require('./cors')
+const cors = require('cors')
 
+server.use(logger('dev'));
+server.use(cors())
 server.use( bodyParser.urlencoded({extended: true}) )
 server.use( bodyParser.json() )
-server.use(allowCors)
 
 server.listen(port, function(){
     console.log("Listening on port: "+ port)
